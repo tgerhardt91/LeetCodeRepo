b3704465f5dcc6aef36035ca6f57e5847bae2481
tgerhardt <tgerhardt91@gmail.com>
2018-11-10 14:16:13 -0600

    Added src folder

---
+++ src/LeetCodeRepo/Solutions/IAmALeetCodeSolution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeRepo.Solutions
+{
+    public interface IAmALeetCodeSolution
+    {
+        bool Execute();
+        int SolutionId();
+        string Difficulty();
+    }
+}
